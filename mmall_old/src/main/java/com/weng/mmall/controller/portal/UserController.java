29506610cc1220b693e76844dd80a54dede5036c
weng2513 <wengxiaozhi2008@gmail.com>
2021-09-26 21:18:43 +0900

    study:messageSource

--- mmall_old/src/main/java/com/weng/mmall/controller/portal/UserController.java
+++ mmall_old/src/main/java/com/weng/mmall/controller/portal/UserController.java
@@ -1,11 +1,15 @@
 package com.weng.mmall.controller.portal;
 
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.weng.mmall.service.IUserService;
 
@@ -15,11 +19,20 @@ public class UserController {
 	@Autowired
     private IUserService iUserService;
 	
+	@Autowired
+	private MessageSource msgSource;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class); 
+	
 //    @RequestMapping(value = "login", method = RequestMethod.POST)
     @GetMapping("login")
     public String login(){
     	String username = "admin";
     	String password ="427338237BD929443EC5D48E24FD2B1A";
+    	
+    	// messageの設定、取得の学び
+    	LOGGER.debug("msg1: {}", msgSource.getMessage("msg1", null, Locale.CHINESE));
+    	
         return iUserService.login(username, password);
     }
 }
